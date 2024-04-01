9c64ea06ccff4ec0edf4d7425d5f836ec9164464
Your Name <you@example.com>
2024-04-01 18:46:19 +0100

    added course disenrolment route and linked to front

--- routes/enrolledCourses.js
+++ routes/enrolledCourses.js
@@ -46,4 +46,45 @@ router.patch("/enrolled/:id", (request, response) => {
   response.send({ code: 1, message: "Enrolled on course" });
 });
 
+router.delete("/enrolled/:id/:courseId", (request, response) => {
+  const { id, courseId } = request.params;
+  const { users } = request;
+
+  console.log(id, courseId);
+  const idAsNumber = Number(id);
+
+  //   check that user ID exists and is a number
+  if (!id || isNaN(idAsNumber)) {
+    response.send("Not a valid ID number");
+    return;
+  }
+
+  //   check that user ID being checked matches one in database
+  const indexOfUser = users.findIndex((item) => {
+    return item.id === idAsNumber;
+  });
+
+  //   if not in database, send error message
+  if (indexOfUser === -1) {
+    response.send("Account not found");
+    return;
+  }
+
+  //   destructure enrolledCourses from selected user
+  const { enrolledCourses } = users[indexOfUser];
+
+  //   if course is already enrolled, show error
+  const courseIndex = enrolledCourses.findIndex((item) => {
+    return item.id === Number(courseId);
+  });
+
+  if (courseIndex === -1) {
+    response.send({ code: 0, message: "Course not found in enrolled courses" });
+    return;
+  }
+
+  users[indexOfUser].enrolledCourses.splice(courseIndex, 1);
+  response.send({ code: 1, message: "User has unenrolled from course" });
+});
+
 module.exports = router;
