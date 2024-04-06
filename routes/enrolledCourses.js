24b3c5e1b6033bf1c4e428bb2174da06f6de9ecd
Harry <harry_bannister@outlook.com>
2024-04-06 18:41:34 +0100

    delete enrolledCourse  route modified

--- routes/enrolledCourses.js
+++ routes/enrolledCourses.js
@@ -31,45 +31,25 @@ router.patch("/enrolled", verifyToken, (request, response) => {
   }
 });
 
-router.delete("/enrolled/:id/:courseId", (request, response) => {
-  const { id, courseId } = request.params;
-  const { users } = request;
+router.delete("/deleteEnrolled/:courseId", verifyToken, (request, response) => {
+  const { courseId } = request.params;
+  const { verifiedUser } = request;
 
-  console.log(id, courseId);
-  const idAsNumber = Number(id);
+  const courseNumber = Number(courseId);
 
-  //   check that user ID exists and is a number
-  if (!id || isNaN(idAsNumber)) {
-    response.send("Not a valid ID number");
-    return;
-  }
-
-  //   check that user ID being checked matches one in database
-  const indexOfUser = users.findIndex((item) => {
-    return item.id === idAsNumber;
-  });
-
-  //   if not in database, send error message
-  if (indexOfUser === -1) {
-    response.send("Account not found");
-    return;
-  }
-
-  //   destructure enrolledCourses from selected user
-  const { enrolledCourses } = users[indexOfUser];
-
-  //   if course is already enrolled, show error
-  const courseIndex = enrolledCourses.findIndex((item) => {
-    return item.id === Number(courseId);
+  const courseIndex = verifiedUser.enrolledCourses.findIndex((item) => {
+    return item.id === courseNumber;
   });
 
   if (courseIndex === -1) {
-    response.send({ code: 0, message: "Course not found in enrolled courses" });
+    response.send("Course not found");
     return;
   }
-
-  users[indexOfUser].enrolledCourses.splice(courseIndex, 1);
-  response.send({ code: 1, message: "User has unenrolled from course" });
+  if (courseIndex >= 0) {
+    verifiedUser.enrolledCourses.splice(courseIndex, 1);
+    response.send({ code: 1, message: "User has unenrolled from course" });
+    return;
+  }
 });
 
 module.exports = router;
