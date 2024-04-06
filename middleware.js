092fdb345cb79267ed88a9e8b1bcc9945dca23fb
Your Name <you@example.com>
2024-04-06 11:58:26 +0100

    added delete user functionality to delete route

--- middleware.js
+++ middleware.js
@@ -1,8 +1,14 @@
 function verifyToken(request, response, next) {
   const user = request.users.find((user) => {
-    return user.token.includes(request.headers.token);
+    // if there is a user with tokens, look for a token in the token array that matches token in headers
+    if (user.token) {
+      return user.token.find((item) => {
+        return item === request.headers.token;
+      });
+    }
+    // skips users with no tokens
+    return false;
   });
-
   if (user) {
     request.verifiedUser = user;
     next();
