95e3eea52fb0500d7302a5f2276bb54d89b09d12
Your Name <you@example.com>
2024-04-15 20:03:46 +0100

    linked all user account settings to database

--- routes/getUser.js
+++ routes/getUser.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const { verifyToken } = require("../middleware");
+const mySQL = require("../mysql/driver");
 const router = express.Router();
+const { getUser } = require("../mysql/queries");
 
 router.get("/", (request, response) => {
   const { users } = request;
@@ -12,8 +13,20 @@ router.get("/", (request, response) => {
   response.send(users);
 });
 
-router.get("/getUser", verifyToken, (request, response) => {
-  response.send({ code: 1, user: request.verifiedUser });
+router.get("/getUser", async (request, response) => {
+  const { token } = request.headers;
+  const results = await mySQL(getUser(token));
+
+  if (results.length > 0) {
+    response.send({
+      code: 1,
+      user: {
+        email: results[0].email,
+        username: results[0].username,
+        password: results[0].password,
+      },
+    });
+  }
 });
 
 module.exports = router;
