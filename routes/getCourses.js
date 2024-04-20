518dc6e5d0b12f3c8d15706f18f85052b19883ec
Harry <harry_bannister@outlook.com>
2024-04-20 13:31:14 +0100

    work on getCourses route and queries

--- routes/getCourses.js
+++ routes/getCourses.js
@@ -1,48 +1,85 @@
 const express = require("express");
 const router = express.Router();
-const courses = require("../courseContent.json");
+// const courses =require("../courseContent.json");
 const { verifyToken } = require("../middleware");
+const mySQL = require("../mysql/driver");
+const {
+  getCourses,
+  getContent,
+  getModules,
+  getCourse,
+} = require("../mysql/queries");
 
-router.get("/", (request, response) => {
-  const { id, title } = request.query;
-  const idAsNumber = Number(id);
+router.get("/", async (request, response) => {
+  // const { id } = request.params;
+  // const idAsNumber = Number(id);
 
-  let copy = [...courses];
+  const courses = await mySQL(getCourses());
+  // console.log(courses);
 
-  if (idAsNumber) {
-    copy = copy.filter((course) => {
-      return idAsNumber === course.id;
-    });
-  }
+  // const content = await mySQL(getContent());
+  // courses.map((item) => {
+  //   console.log(content);
+  // });
 
-  if (title) {
-    copy = copy.filter((course) => {
-      return course.title.toLowerCase().includes(title.toLowerCase());
-    });
-  }
+  // const content = courses.map((item) => {
+  //   return {
+  //     course_title: item.course_title,
+  //     type: item.type,
+  //     content: item.content,
+  //   };
+  // });
 
-  response.send({ code: 1, content: copy });
+  response.send({ code: 1, courses: courses });
+  return;
+
+  // let copy = [...courses];
+
+  // if (idAsNumber) {
+  //   copy = copy.filter((course) => {
+  //     return idAsNumber === course.id;
+  //   });
+  // }
+
+  // if (title) {
+  //   copy = copy.filter((course) => {
+  //     return course.title.toLowerCase().includes(title.toLowerCase());
+  //   });
+  // }
+
+  // response.send({ code: 1, content: copy });
 });
 
-router.get("/:id", (request, response) => {
-  const { id, title } = request.query;
-  const idAsNumber = Number(id);
+router.get("/:id", async (request, response) => {
+  const { id } = request.params;
 
-  let copy = [...courses];
+  const course = await mySQL(getCourse(id));
+  const modules = await mySQL(getModules(id));
 
-  if (idAsNumber) {
-    copy = copy.filter((course) => {
-      return idAsNumber === course.id;
-    });
+  course[0].modules = modules;
+
+  for (let i = 0; i < course[0].modules.length; i++) {
+    const content = await mySQL(getContent(course[0].modules[i].id));
+    course[0].modules[i].content = content;
   }
 
-  if (title) {
-    copy = copy.filter((course) => {
-      return course.title.toLowerCase().includes(title.toLowerCase());
-    });
-  }
+  console.log(course[0]);
 
-  response.send({ code: 1, content: copy });
+  // // let copy = [...courses];
+
+  // // if (idAsNumber) {
+  // //   copy = copy.filter((course) => {
+  // //     return idAsNumber === course.id;
+  // //   });
+  // // }
+
+  // // if (title) {
+  // //   copy = copy.filter((course) => {
+  // //     return course.title.toLowerCase().includes(title.toLowerCase());
+  // //   });
+  // // }
+
+  response.send({ code: 1, course: course[0] });
 });
 
 module.exports = router;
