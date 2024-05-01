5ca6d6bdc0846cedbcfc6773549dedcde46f8b76
Your Name <you@example.com>
2024-05-01 21:40:26 +0100

    added smtp functionality

---
+++ email/nodemailer.js
@@ -0,0 +1,26 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: `mail.we-learn.uk`,
+  port: 465,
+  secure: true, //allow use of port 587 must be true if  port 465
+  auth: {
+    user: `help@we-learn.uk`,
+    pass: `aUL?VIJaMJ@N`,
+  },
+});
+
+function sendEmail(payload, sender, to) {
+  const mailOptions = {
+    from: `help@we-learn.uk`,
+    to: to[0].email,
+    subject: payload.subject,
+    html: payload.content,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    console.log(error, info);
+  });
+}
+
+module.exports = { sendEmail };
---
+++ email/templates.js
@@ -0,0 +1,30 @@
+const welcomeEmail = (email) => {
+  return {
+    subject: `Welcome to We-Learn`,
+    content: `<div>
+                <h4>Welcome ${email}</h4>
+                <p>Thank you for signing up!</p>
+            </div>`,
+  };
+};
+
+const accountDelete = (email) => {
+  return {
+    subject: `Account deleted`,
+    content: `<div>
+                <h1>Sorry to see you go ${email}</h1>
+                <p>Your account is deleted!</p>
+            </div>`,
+  };
+};
+
+const userDetailsUpdate = () => {
+  return {
+    subject: `Details updated`,
+    content: `<div>
+                    <h4>Your personal details have been successfully changed</h4>
+                    </div>`,
+  };
+};
+
+module.exports = { welcomeEmail, accountDelete };
