4bb35d3f0d509199f5d40962db852f9e147733f9
Harry <harry_bannister@outlook.com>
2024-06-13 23:01:52 +0100

    changes

--- email/nodemailer.js
+++ email/nodemailer.js
@@ -1,31 +1,31 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: 465,
-  secure: true, //allow use of port 587 must be true if port 465
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-});
-function sendEmail(payload, sender, to) {
-  const mailOptions = {
-    from: sender === undefined ? "help@we-learn.uk" : sender,
-    to: to[0].email,
-    subject: payload.subject,
-    html: payload.content,
-  };
+// const transporter = nodemailer.createTransport({
+//   host: process.env.EMAIL_HOST,
+//   port: 465,
+//   secure: true, //allow use of port 587 must be true if port 465
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASSWORD,
+//   },
+// });
+// function sendEmail(payload, sender, to) {
+//   const mailOptions = {
+//     from: sender === undefined ? "help@we-learn.uk" : sender,
+//     to: to[0].email,
+//     subject: payload.subject,
+//     html: payload.content,
+//   };
 
-  try {
-    console.log("login attempted");
+//   try {
+//     console.log("login attempted");
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      console.log(error, info);
-    });
-  } catch (error) {
-    console.error("Error sending email", error);
-  }
-}
+//     transporter.sendMail(mailOptions, (error, info) => {
+//       console.log(error, info);
+//     });
+//   } catch (error) {
+//     console.error("Error sending email", error);
+//   }
+// }
 
-module.exports = { sendEmail };
+// module.exports = { sendEmail };
