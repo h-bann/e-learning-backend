fc40a6a960cdbd337b26c893e4f9b444da661149
Harry <harry_bannister@outlook.com>
2024-10-10 16:41:57 +0100

    changes to gitignore

--- server.js
+++ server.js
@@ -7,27 +7,20 @@ const cors = require("cors");
 const helmet = require("helmet");
 const { rateLimit } = require("express-rate-limit");
 
-const rateLimiter = rateLimit({
-  windowMs: 900000, // 15 mins
-  limit: 100000, // number of requests
-});
-app.use((req, res, next) => {
-  console.log(req.headers);
-  console.log("Origin:", req.headers.origin);
-  next();
-});
-
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://welearning.uk"],
+  origin: ["https://welearning.uk"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 };
 app.use(cors(corsOptions));
 
+const rateLimiter = rateLimit({
+  windowMs: 900000, // 15 mins
+  limit: 50, // number of requests
+});
 app.use(rateLimiter);
 
 app.use(helmet());
 app.use(express.static("public_html"));
-
 app.use(express.json());
 
 app.use("/users", require("./routes/addUser"));
@@ -39,10 +32,6 @@ app.use("/users", require("./routes/logoutUser"));
 app.use("/courses", require("./routes/getCourses"));
 app.use("/courses", require("./routes/enrolledCourses"));
 app.use("/contact", require("./routes/contact"));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public_html", "index.html"));
-});
 
 const PORT = process.env.PORT || 6001;
 app.listen(PORT, () => {
